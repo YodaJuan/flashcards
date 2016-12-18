9a95b7363ba505a228d952d579b80d8e68c653e2
YodaJuan <keilah2455@hotmail.com>
2016-12-17 20:19:28 -0500

    App server loads public directory i Please enter the commit message for your changes. Lines starting

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var app = express();
+var express = require('express');  //  including the express library in the file
+var app = express(); 	// instantiating the express library
+// serve up the public folder as a default route``
+app.use(express.static(__dirname + '/public'));
 
-app.get ('/', function(request, response) {
-	response.send ('Hello World');
-})
+//app.get ('/', function(request, response) {
+//	response.send ('Hello World');
+//})
 
 app.listen(3000, function() {
 	console.log('server running on port 3000')
